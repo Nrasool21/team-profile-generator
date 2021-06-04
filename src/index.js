957e74cd697c146b189a32b8e21f3a7b38ef40ce
Nazia R <nazia.huma@gmail.com>
2021-06-04 12:23:58 +0100

    pushed manager details into the employees array

--- src/index.js
+++ src/index.js
@@ -1,50 +1,54 @@
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
 
+const Manager = require("./lib/manager");
+
+const employees = [];
 
 const addManager = async () => {
+  const managerQuestions = [
+    {
+      type: "input",
+      message: "What is the name of your manager?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Please enter manager's email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your manager's employee ID?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your manager's office number?",
+      name: "officeNumber",
+    },
+  ];
 
-    const managerQuestion = [
-      {
-        type: "input",
-        message: "What is the name of your manager?",
-        name: "name",
-      },
-      {
-        type: "input",
-        message: "Please enter manager's email address?",
-        name: "email",
-      },
-      {
-        type: "input",
-        message: "What is your manager's employee ID?",
-        name: "id",
-      },
-      {
-        type: "input",
-        message: "What is your manager's office number?",
-        name: "officeNumber",
-      },
-    ];
+  const answers = await inquirer.prompt(managerQuestions);
 
-    const answers = await inquirer.prompt(managerQuestion)
-  
-    console.log(answers)
-    
+  const manager = new Manager(
+    answers.id,
+    answers.name,
+    answers.email,
+    answers.officeNumber
+  );
+
+  employees.push(manager);
 };
 
-const addEmployees = () => {
+const addEmployees = () => {};
 
-}
+const generateHtml = () => {};
 
-const generateHtml = () => {
+const writeToFile = () => {};
 
-}
-
-const writeToFile = () => {
-
-}
-
-const init = async() => {
-    await addManager()
-}
-init ()
+const init = async () => {
+  
+  await addManager();
+  
+};
+init();
--- src/lib/manager.js
+++ src/lib/manager.js
@@ -3,7 +3,7 @@ const Employee = require("./employee");
 class Manager extends Employee {
   constructor(id, name, email, officeNumber) {
     super(id, name, email);
-    this.officeNumber;
+    this.officeNumber = officeNumber;
   }
   
   getOfficeNumber() {
