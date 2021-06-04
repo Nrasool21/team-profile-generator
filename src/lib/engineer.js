58b700e71f8a6475f28569889e79e96616875048
Nazia R <nazia.huma@gmail.com>
2021-06-04 11:49:56 +0100

    consructed all four classes using this operation

--- src/lib/engineer.js
+++ src/lib/engineer.js
@@ -1,7 +1,14 @@
 const Employee = require("./employee");
 
-class Engineer extends Employee{
-    constructor (){}
+class Engineer extends Employee {
+  constructor(id, name, email, github) {
+    super(id, name, email);
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
 }
 
-module.exports = Engineer; 
+module.exports = Engineer;
